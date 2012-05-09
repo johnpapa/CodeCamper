079f19ee46fbe787f930b5bd54f0d12590e04143
Ward Bell <wardb@ideablade.com>
2012-05-08 23:26:15 -0700

    Refactored CodeCamperDbContext configuration into configuration classes, SessionConfiguration.cs and PersonSessionConfiguration.cs at John's request.
    Deleted the .gitignore hiding in CodeCamper.Web/App_Data directory (how did it get there in the first place?)

---
+++ CodeCamper.Data/PersonSessionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using CodeCamper.Model;
+
+namespace CodeCamper.Data
+{
+    class PersonSessionConfiguration : EntityTypeConfiguration<PersonSession>
+    {
+        public PersonSessionConfiguration()
+        {
+            HasKey(ps => new { ps.SessionId, ps.PersonId });
+
+            HasRequired(ps => ps.Session)
+                .WithMany(s => s.AttendeeSessions)
+                .HasForeignKey(ps => ps.SessionId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(ps => ps.Person)
+                .WithMany(p => p.AttendingSessions)
+                .HasForeignKey(ps => ps.PersonId)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
---
+++ CodeCamper.Data/SessionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using CodeCamper.Model;
+
+namespace CodeCamper.Data
+{
+    class SessionConfiguration : EntityTypeConfiguration<Session>
+    {
+        public SessionConfiguration()
+        {
+            HasRequired(s => s.Speaker)
+               .WithMany(p => p.SpeakerSessions)
+               .HasForeignKey(s => s.SpeakerId);
+        }
+    }
+}
