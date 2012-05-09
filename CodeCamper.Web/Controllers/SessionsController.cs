10e179312a29129b87549c3dc262a68da7e79457
Ward Bell <wardb@ideablade.com>
2012-05-08 22:40:24 -0700

    Refactored CampController to be pure Lookups. Since that's what it is, renamed to LookupsController which should be much easier to explain in the course. Actions are:
    api/lookups
    api/lookups/lookups (same)
    api/lookups/rooms
    api/lookups/tracks
    api/lookups/timeslots
    
    Revised routes in Global.asax.cs to use Lookups and the specific Action routes in the SessionController:
    
    api/sessions/briefs
    api/sessions/taggroups
    
    Revised route comments for clarity.

--- CodeCamper.Web/Controllers/SessionsController.cs
+++ CodeCamper.Web/Controllers/SessionsController.cs
@@ -33,5 +33,25 @@ namespace CodeCamper.Web.Controllers.Controllers
             if (session != null) return session;
             throw new HttpResponseException(HttpStatusCode.NotFound);
         }
+
+        #region Actions (RPC methods)
+
+        // These actions are invoked by specific routes defined in the Global.asax
+
+        // GET: api/sessions/briefs
+        [ActionName("briefs")]
+        public IQueryable<SessionBrief> GetSessionBriefs()
+        {
+            return DataService.Sessions.SessionBriefs().OrderBy(sb => sb.TimeSlotId);
+        }
+
+        // GET: api/sessions/taggroups
+        [ActionName("taggroups")]
+        public IQueryable<TagGroup> GetTagGroups()
+        {
+            return DataService.Sessions.TagGroups().AsQueryable();
+        }
+
+        #endregion
     }
 }
