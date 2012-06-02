8b6a724add77190b13062930868770c137d7b38a
Ward Bell <wardb@ideablade.com>
2012-06-02 12:13:11 -0700

    Uow is disposed when Web API disposes the IoC controller so don't need to dispose in the ApiBaseController Commented out that dispose and confirmed that Uow is disposed automatically. ApiBaseController now only has a Uow property; strongly consider deleting this class and having our controllers derive directly from ApiController while each has its own Uow property. Talk to John first.

--- CodeCamper.Web/Controllers/ApiBaseController.cs
+++ CodeCamper.Web/Controllers/ApiBaseController.cs
@@ -6,21 +6,23 @@ using CodeCamper.Model;
 
 namespace CodeCamper.Web.Controllers
 {
+    // TODO: Remove this class and relocate Uow property to the derived controllers?
+    //       It isn't doing enough now to justify it's existence.
     public abstract class ApiControllerBase : ApiController
     {
         protected ICodeCamperUow Uow { get; set; }
 
-        // base ApiController is IDisposable
-        // Dispose of the repository if it is IDisposable
-        protected override void Dispose(bool disposing)
-        {
-            if (Uow != null && Uow is IDisposable)
-            {
-                ((IDisposable)Uow).Dispose();
-                Uow = null;
-            }
-            base.Dispose(disposing);
-        }
+        //// base ApiController is IDisposable
+        //// Dispose of the repository if it is IDisposable
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (Uow != null && Uow is IDisposable)
+        //    {
+        //        ((IDisposable)Uow).Dispose();
+        //        Uow = null;
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 
 }
