e20e98ec80baf30e0c9150ec005d1c248c257f80
John Papa <john@johnpapa.net>
2012-06-11 21:51:49 -0400

    added change tracker

---
+++ CodeCamper.Web/Scripts/lib/knockout.changetracker-amd.js
@@ -0,0 +1,63 @@
+﻿// Knockout.ChangeTracker
+//
+// John Papa 
+//          http://johnpapa.net
+//          http://twitter.com/@john_papa
+//
+// Depends on scripts:
+//          Knockout 
+//
+//  Notes:
+//          Special thanks to Steve Sanderson for his influence and help on
+//          this change tracker.
+//
+//  Usage:      
+//          To Setup Tracking, add this tracker property to your viewModel    
+//              ===> viewModel.tracker = new ChangeTracker(viewModel);
+//
+//          Hook these into your view ...
+//              Did It Change?          
+//              ===> viewModel.tracker().isDirty();
+//
+//          Hook this into your view model functions (ex: load, save) ...
+//              Resync Changes
+//              ===> viewModel.tracker().reset;
+//
+//          Optionally, you can pass your own hashFunction for state tracking.
+////////////////////////////////////////////////////////////////////////////////////////
+define(['ko'], function(ko) {
+    //    (function (ko) {
+    'use strict';
+    ko.DirtyFlag = function (objectToTrack, isInitiallyDirty, hashFunction) {
+
+        hashFunction = hashFunction || ko.toJSON;
+
+        var
+            _objectToTrack = objectToTrack,
+            _lastCleanState = ko.observable(hashFunction(_objectToTrack)),
+            _isInitiallyDirty = ko.observable(isInitiallyDirty),
+
+            result = function () {
+                var self = this;
+                self.isDirty = ko.computed(function () {
+                    return _isInitiallyDirty() || hashFunction(_objectToTrack) !== _lastCleanState();
+                });
+                self.reset = function () {
+                    _lastCleanState(hashFunction(_objectToTrack));
+                    _isInitiallyDirty(false);
+                };
+                return self;
+            };
+    //result.isDirty = ko.computed(function () {
+    //        return hashFunction(objectToTrack) !== lastCleanState();
+    //    });
+
+    //    result.reset = function() {
+    //        lastCleanState(hashFunction(objectToTrack));
+    //    };
+            
+        return result;
+    };
+
+//}(ko));
+});
