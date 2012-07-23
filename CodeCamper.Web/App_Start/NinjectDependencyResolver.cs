4d0e07d54575da72b4405d69ce8d6713584450c5
John Papa <john@johnpapa.net>
2012-07-22 23:11:36 -0400

    separated NinjectDependencyScope class to its own file

--- CodeCamper.Web/App_Start/NinjectDependencyResolver.cs
+++ CodeCamper.Web/App_Start/NinjectDependencyResolver.cs
@@ -1,49 +1,9 @@
-﻿namespace CodeCamper.Web
+﻿using System.Web.Http.Dependencies;
+using CodeCamper.Web.App_Start;
+using Ninject;
+
+namespace CodeCamper.Web
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics.Contracts;
-    using System.Web.Http.Dependencies;
-    using Ninject;
-    using Ninject.Syntax;
-
-    public class NinjectDependencyScope : IDependencyScope
-    {
-        private IResolutionRoot resolver;
-
-        internal NinjectDependencyScope(IResolutionRoot resolver)
-        {
-            Contract.Assert(resolver != null);
-
-            this.resolver = resolver;
-        }
-
-        public void Dispose()
-        {
-            var disposable = resolver as IDisposable;
-            if (disposable != null)
-                disposable.Dispose();
-
-            resolver = null;
-        }
-
-        public object GetService(Type serviceType)
-        {
-            if (resolver == null)
-                throw new ObjectDisposedException("this", "This scope has already been disposed");
-
-            return resolver.TryGet(serviceType);
-        }
-
-        public IEnumerable<object> GetServices(Type serviceType)
-        {
-            if (resolver == null)
-                throw new ObjectDisposedException("this", "This scope has already been disposed");
-
-            return resolver.GetAll(serviceType);
-        }
-    }
-
     public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
     {
         private IKernel kernel;
@@ -59,5 +19,4 @@
             return new NinjectDependencyScope(kernel.BeginBlock());
         }
     }
-
 }
---
+++ CodeCamper.Web/App_Start/NinjectDependencyScope.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Web.Http.Dependencies;
+using Ninject;
+using Ninject.Syntax;
+
+namespace CodeCamper.Web.App_Start
+{
+    public class NinjectDependencyScope : IDependencyScope
+    {
+        private IResolutionRoot resolver;
+
+        internal NinjectDependencyScope(IResolutionRoot resolver)
+        {
+            Contract.Assert(resolver != null);
+
+            this.resolver = resolver;
+        }
+
+        public void Dispose()
+        {
+            var disposable = resolver as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+
+            resolver = null;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
+
+            return resolver.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
+
+            return resolver.GetAll(serviceType);
+        }
+    }
+}
