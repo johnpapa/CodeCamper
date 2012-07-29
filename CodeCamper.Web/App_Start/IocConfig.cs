dbeef178673f05497457334519336e88693773f6
John Papa <john@johnpapa.net>
2012-07-28 23:48:20 -0400

    removed a todo

--- CodeCamper.Web/App_Start/IocConfig.cs
+++ CodeCamper.Web/App_Start/IocConfig.cs
@@ -7,13 +7,13 @@ namespace CodeCamper.Web
 {
     public class IocConfig
     {
-        // PAPA: Added "Configure" in order to introduce IoC and JsonNet serializer
         public static void RegisterIoc(HttpConfiguration config)
         {
             var kernel = new StandardKernel(); // Ninject IoC
 
             // These registrations are "per instance request".
             // See http://blog.bobcravens.com/2010/03/ninject-life-cycle-management-or-scoping/
+
             kernel.Bind<RepositoryFactories>().To<RepositoryFactories>()
                 .InSingletonScope();
 
