17a53d9f609ee94d04daeee7a01a6f3d293a9300
John Papa <john@johnpapa.net>
2012-08-17 23:41:43 -0400

    Added mapping to ajax test/ajax.session.tests.js

--- CodeCamper.Web/test/ajax.session.tests.js
+++ CodeCamper.Web/test/ajax.session.tests.js
@@ -1,7 +1,7 @@
 ﻿define(
     'ajax.session-tests-function',
-    ['jquery'],
-    function ($) {
+    ['jquery', 'ko', 'model', 'model.mapper'],
+    function ($, ko, model, mapper) {
 
         module('AJAX Calls');
 
@@ -15,6 +15,12 @@
                         ok(true, 'GET succeeded for ' + url);
                         ok(!!result, 'GET retrieved some data');
                         //debugger;
+                        var underlyingArray = [];
+                        for (var i = 0; i < result.length; i++) {
+                            underlyingArray.push(mapper.session.fromDto(result[i]));
+                        }
+                        var sessions = ko.observableArray(underlyingArray);
+                        equal(result[0].title, sessions()[0].title(), 'Model mapped properly');
                         start();
                     },
                     error: function (result) {
